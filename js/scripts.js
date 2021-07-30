fcb27581dc629d1b0f403b7110cf1bc0f32c23b8
mr-jingo <85439298+mr-jingo@users.noreply.github.com>
2021-07-30 20:45:13 +0200

    old Code deleted, variable added

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,5 @@
-alert('Hello world');
-let favoriteFood = "Tacos";
-document.write(favoriteFood);
+let pokemonList = [
+  {name: "Pikachu", height: 2, type: ["Elektro", "Battle"]},
+  {name: "Bisasam", height: 10, type: ["Plant", "Water"]},
+  {name: "Pummeluf", height: 8, type: ["Psycho", "Grass"]}
+];
