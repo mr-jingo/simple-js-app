bd8b7797de7ecb47fd4b6ae9d9b06321111652dc
mr-jingo <85439298+mr-jingo@users.noreply.github.com>
2021-08-02 20:33:54 +0200

    error corrected

--- js/scripts.js
+++ js/scripts.js
@@ -6,7 +6,7 @@ let pokemonList = [
 
 for(i = 0; i < pokemonList.length; i++) {
   if(pokemonList[i].height > 8) {
-    document.write(pokemonList[i].name + " " + "(height: " + pokemonList[i].height + ", 'WOW, thats's big!''), ");
+    document.write(pokemonList[i].name + " " + "(height: " + pokemonList[i].height + ", 'WOW, that's big!''), ");
   } else {
     document.write(pokemonList[i].name + " " + "(height: " + pokemonList[i].height + "), ");
   }
