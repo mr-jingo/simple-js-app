b063318a24cfef1128ca1d1f52413468eb9f967e
mr-jingo <85439298+mr-jingo@users.noreply.github.com>
2021-08-02 20:17:38 +0200

    For-Loop implemented
    prints name and height

--- js/scripts.js
+++ js/scripts.js
@@ -3,3 +3,7 @@ let pokemonList = [
   {name: "Bisasam", height: 10, type: ["Plant", "Water"]},
   {name: "Pummeluf", height: 8, type: ["Psycho", "Grass"]}
 ];
+
+for(i = 0; 0 < pokemonList.length; i++) {
+  document.write(`${pokemonList.name} (height: ${pokemonList.height})`);
+}
