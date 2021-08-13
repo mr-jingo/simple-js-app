200ac0265bdc9fd8de6f2b22ef76c180802cc7b4
mr-jingo <85439298+mr-jingo@users.noreply.github.com>
2021-08-13 12:52:24 +0200

    changes for Task 1.5

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,24 @@
-let pokemonList = [
-  {name: "Pikachu", height: 2, type: ["Elektro", "Battle"]},
-  {name: "Bisasam", height: 10, type: ["Plant", "Water"]},
-  {name: "Pummeluf", height: 8, type: ["Psycho", "Grass"]}
-];
+let pokemonRepository = (function () {
+  let pokemonList = [
+    {name: "Pikachu", height: 2, type: ["Elektro", "Battle"]},
+    {name: "Bisasam", height: 10, type: ["Plant", "Water"]},
+    {name: "Pummeluf", height: 8, type: ["Psycho", "Grass"]}
+  ];
 
-for(i = 0; i < pokemonList.length; i++) {
-  if(pokemonList[i].height > 8) {
-    document.write(pokemonList[i].name + " " + "(height: " + pokemonList[i].height + ", 'WOW, that's big!''), ");
-  } else {
-    document.write(pokemonList[i].name + " " + "(height: " + pokemonList[i].height + "), ");
+  function add(pokemon) {
+    pokemonList.push(pokemon);
   }
-}
+
+  function getAll() {
+    return pokemonList;
+  }
+
+  return {
+    add: add,
+    getAll: getAll
+  };
+})();
+
+pokemonRepository.getAll().forEach(function(pokemon) {
+  document.write(pokemon.name + " " + "(height: " + pokemon.height + ", type: " + pokemon.type + ")" + "<br>");
+})
