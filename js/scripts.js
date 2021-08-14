169eccb54f0c659321e45a733dea542a187c1e8e
mr-jingo <85439298+mr-jingo@users.noreply.github.com>
2021-08-14 22:23:55 +0200

    new style for list-button / added h1 / bugfix classList.add

--- js/scripts.js
+++ js/scripts.js
@@ -34,7 +34,7 @@ pokemonRepository.getAll().forEach(function(pokemon) {
   let listItem = document.createElement('li');
   let button = document.createElement('button');
   button.innerText = pokemon.name;
-  button.classList.add(".list-button");
+  button.classList.add("list-button");
   listItem.appendChild(button);
   list.appendChild(listItem);
 })
